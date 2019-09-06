bae43e5e7b1d43ead70907f8ca07ca138e9bff14
Prism97 <priyetasaha@gmail.com>
2019-09-06 16:00:37 +0600

    Working Version 5, stack logic added

--- input.asm
+++ input.asm
@@ -1,11 +1,9 @@
-addi $t1, $t1, 5
-sll $zero, $t1, 2
-srl $zero, $t1, 2
-j 7
+addi $t1, $t1, 2
+sw $t1, 100($zero)
 addi $t0, $t0, 3
-sw $t0, 100($sp)
-lw $t1, 100($sp)
-subi $t1, $t1, 1
-sw $t1, 50($zero)
-lw $t2, 50($zero)
-add $t3, $t0, $t2
+sw $t1, 0($sp)
+sw $t0, 4($sp)
+subi $sp, $sp, 4
+sw $t0, 4($sp)
+lw $t3, 0($sp)
+sub $t3, $t3, $zero
